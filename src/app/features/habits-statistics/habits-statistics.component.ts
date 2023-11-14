1ca312b2d99ff679488be919839b3baa536a79e9
Paweł Ciszewski <kardahim@gmail.com>
2023-11-14 19:24:58 +0100

    add radar-chart example

--- src/app/features/habits-statistics/habits-statistics.component.ts
+++ src/app/features/habits-statistics/habits-statistics.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarChartComponent } from './ui/bar-chart/bar-chart.component';
+import { RadarChartComponent } from './ui/radar-chart/radar-chart.component';
 
 @Component({
   selector: 'app-habits-statistics',
   standalone: true,
-  imports: [CommonModule, BarChartComponent],
+  imports: [CommonModule, BarChartComponent, RadarChartComponent],
   templateUrl: './habits-statistics.component.html',
   styleUrls: ['./habits-statistics.component.scss'],
 })
