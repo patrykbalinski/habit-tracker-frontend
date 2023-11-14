ddaa03310c28e8e452c85bf82b6908aeb17d63e0
Paweł Ciszewski <kardahim@gmail.com>
2023-11-14 19:09:58 +0100

    add bar-chart example

--- src/app/features/habits-statistics/habits-statistics.component.ts
+++ src/app/features/habits-statistics/habits-statistics.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BarChartComponent } from './ui/bar-chart/bar-chart.component';
 
 @Component({
   selector: 'app-habits-statistics',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BarChartComponent],
   templateUrl: './habits-statistics.component.html',
-  styleUrls: ['./habits-statistics.component.scss']
+  styleUrls: ['./habits-statistics.component.scss'],
 })
-export class HabitsStatisticsComponent {
-
-}
+export class HabitsStatisticsComponent {}
