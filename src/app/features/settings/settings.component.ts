f5079450840278452a0e5e230fa5d2fd407fa27d
patryk.balinski <patrykbalinski00@gmail.com>
2023-11-20 19:38:11 +0100

    improved settings component, improved translations

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from "primeng/button";
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TranslateModule } from "@ngx-translate/core";
+import { Router } from "@angular/router";
+import { DropdownModule } from "primeng/dropdown";
+import { Language } from "@core/interfaces/language";
+import { TranslationsService } from "@core/services/translations.service";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 
+@UntilDestroy()
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    DropdownModule,
+    FormsModule
+  ],
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
 
+  private router: Router = inject(Router);
+  public translations: TranslationsService = inject(TranslationsService);
+  public languageFormController: FormControl = new FormControl(this.translations.getCurrentLanguage());
+
+  public ngOnInit():void {
+    this.initDataHandle();
+  }
+
+  public onSave(): void {
+    console.log('save settings in backend');
+  }
+
+  public onCancel(): void {
+    this.router.navigate(['', 'habits']).then();
+  }
+
+  private initDataHandle(): void {
+    this.languageFormController.valueChanges
+      .pipe(untilDestroyed(this))
+      .subscribe((languageCode: Language['code']) => this.translations.changeLanguage(languageCode));
+  }
 }
